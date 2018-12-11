b16290cf067a55220d03f7854ccc87f49f967d26
joel-lugo <45578539+joel-lugo@users.noreply.github.com>
2018-12-10 22:02:56 -0500

    Add files via upload

---
+++ online-library correct/inc/data.php
@@ -0,0 +1,180 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "College Algebra Triginometry",
+	"img" => "img/media/math.jpg",
+    "genre" => "Text Book",
+    "format" => "Hard Cover",
+    "year" => 2016,
+    "category" => "Books",
+    "authors" => [
+        "Man M. Sharma",
+        "Madan G. Chopra",
+        "Ralph Johnson",
+        "John Vlissides"
+    ],
+    "publisher" => "Ramesh Thomas",
+    "isbn" => '978-1-888469-97-4'
+];
+$catalog[102] = [
+    "title" => "Milk and honey",
+    "img" => "img/media/milk.jpg",
+    "genre" => "Poetry",
+    "format" => "Soft Cover",
+    "year" => 2014,
+    "category" => "Books",
+    "authors" => [
+        "Various"
+    ],
+    "publisher" => "Andrew McMeel",
+    "isbn" => '9781449474256'
+];
+$catalog[103] = [
+    "title" => "Diary of A Wimpy Kid",
+    "img" => "img/media/kid.jpg",
+    "genre" => "fiction",
+    "format" => "Hardcover",
+    "year" => 2007,
+    "category" => "Books",
+    "authors" => [
+        "Jeff Kinney"
+    ],
+    "publisher" => "Addison-Wesley Professional",
+    "isbn" => '9780810993136'
+];
+$catalog[104] = [
+    "title" => "The Clean Coder: A Code of Conduct for Professional Programmers",
+    "img" => "img/media/joy.jpg",
+    "genre" => "Tech",
+    "format" => "Audio",
+    "year" => 2011,
+    "category" => "Books",
+    "authors" => [
+        "Robert C. Martin"
+    ],
+    "publisher" => "Prentice Hall",
+    "isbn" => '007-6092046981'
+];
+//Movies
+$catalog[201] = [
+    "title" => "Venom",
+    "img" => "img/media/venom.jpg",
+    "genre" => "action",
+    "format" => "DVD",
+    "year" => 2018,
+    "category" => "Movies",
+    "director" => "Ruben Fleischer",
+    "writers" => [
+        "Jeff Pinker",
+        "Kelly Marcel"
+    ],
+    "stars" => [
+        "Tom Hardy",
+        "Michelle Williams",
+        "Riz Ahmed",
+        "woody Harrelson"
+    ]
+];
+$catalog[202] = [
+    "title" => "Nightcrawler",
+    "img" => "img/media/night.jpg",
+    "genre" => "thriller",
+    "format" => "Blu-ray",
+    "year" => 2014,
+    "category" => "Movies",
+    "director" => "Dan Gilroy",
+    "writers" => [
+        "Dan Gilroy"
+    ],
+    "stars" => [
+        "Jake Gyllenhaal",
+        "Riz Ahmed",
+        "Rene Russo",
+        "Rose Gilroy",
+        "Diedrich Bader",
+        "Stephen Root"
+    ]
+];
+$catalog[203] = [
+    "title" => "Avatar",
+    "img" => "img/media/avatar.jpg",
+    "genre" => "science fiction",
+    "format" => "Blu-ray",
+    "year" => 2012,
+    "category" => "Movies",
+    "director" => "James Cameron",
+    "writers" => [
+        "James Cameron"
+
+    ],
+    "stars" => [
+        "Steven spielberg",
+        "kate Winslet",
+        "Ridely Scott",
+        "Arnold Schwarzenegger",
+        "Martin Scorsese",
+        "Zoe Saldana"
+    ]
+];
+$catalog[204] = [
+    "title" => "Captin America: The Winter Soldier ",
+    "img" => "img/media/america.jpg",
+    "genre" => "Action",
+    "format" => "DVD",
+    "year" => 2014,
+    "category" => "Movies",
+    "director" => "Joe Russo",
+    "writers" => [
+        "Stephen McFeely"
+    ],
+    "stars" => [
+        "Chris Evens",
+        "Sebastian Stan",
+        "Scarlett Johansson",
+        "Anthony Mackie",
+        "Emely Vancamp",
+        "Frank Grillo",
+        "André the Giant",
+        "Fred Savage",
+        "Peter Falk",
+        "Billy Crystal"
+    ]
+];
+//Music
+$catalog[301] = [
+    "title" => "Lavish",
+    "img" => "img/media/bbno.jpg",
+    "genre" => "Hip Hop",
+    "format" => "Soundcloud",
+    "year" => 2018,
+    "category" => "Music",
+    "artist" => "bbno$"
+];
+$catalog[302] = [
+    "title" => "Paranoia",
+    "img" => "img/media/dave.jpg",
+    "genre" => "Rap",
+    "format" => "cd",
+    "year" => 2017,
+    "category" => "Music",
+    "artist" => "Dave East"
+];
+$catalog[303] = [
+    "title" => "Novacain",
+    "img" => "img/media/frank.jpg",
+    "genre" => "R&B",
+    "format" => "cd",
+    "year" => 2016,
+    "category" => "Music",
+    "artist" => "Frank Ocean"
+];
+$catalog[304] = [
+    "title" => "Swimming",
+    "img" => "img/media/mac.jpg",
+    "genre" => "Rap",
+    "format" => "Rap",
+    "year" => 2018,
+    "category" => "Music",
+    "artist" => "Mac Miller"
+];
